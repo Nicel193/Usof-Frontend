27bc3899d3e3062fef8977d33d4dc81ba460c6d0
Nicel193 <nicelgoog@gmail.com>
2023-11-29 15:48:24 +0200

    Add draw profile posts

--- my-app/src/layouts/Profile.jsx
+++ my-app/src/layouts/Profile.jsx
@@ -2,22 +2,55 @@ import "./styles/Profile.scss";
 
 import { UilPlus } from "@iconscout/react-unicons";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Post from "../components/Post";
 
 import ProfileInfo from "../components/ProfileInfo";
 
+import { useSelector } from "react-redux";
 import { createPost } from "../api/createPost";
+import { useSearchParams } from "react-router-dom";
+import { getPostsByUserId } from "../api/getPosts";
 
 const Profile = () => {
+  const userData = useSelector((state) => state.auth.userData);
+
+  const [shouldFetchPosts, setShouldFetchPosts] = useState(true);
+  const [posts, setPosts] = useState([]);
   const [post, setPost] = useState({ title: "", content: "", categories: [2] });
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (!searchParams.has("page")) {
+      setSearchParams({ page: "1" });
+    }
+    async function fetchData() {
+      if (!userData) return;
+
+      const response = await getPostsByUserId(
+        userData.id,
+        searchParams.toString()
+      );
+      console.log(response);
+      setPosts(response.data);
+    }
+    
+    fetchData()
+      .then()
+      .catch((e) => console.log(e));
+
+    setShouldFetchPosts(false);
+    
+    //eslint-disable-next-line
+  }, [userData, searchParams, shouldFetchPosts]);
 
   async function shareNewPost() {
     await createPost(post)
       .then((response) => {
         console.log(response);
         setPost({ title: "", content: "", categories: [2] });
+        setShouldFetchPosts(true);
       })
       .catch((e) => {
         console.log(e);
@@ -54,10 +87,19 @@ const Profile = () => {
           </button>
           <span className="yourPostsText">Your Posts</span>
           <div class="underline"></div>
-
-          <Post />
-          <Post />
-          <Post />
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <Post
+                authorLogin={post.authorLogin}
+                title={post.title}
+                content={post.content}
+                categories={post.categories}
+                publishDate={post.publishDate}
+              />
+            ))
+          ) : (
+            <div style={{ fontSize: "32px" }}>Posts not found</div>
+          )}
         </div>
         <ProfileInfo />
       </section>
